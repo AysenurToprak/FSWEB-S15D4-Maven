14a25526927549ec47cfc8e38c7bad36a467d1f2
Ank <toprakaysenur96@gmail.com>
2024-11-26 23:40:05 +0300

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,44 @@
 package org.example;
 
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    public static boolean checkForPalindrome(String text) {
+        String str = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        LinkedList<Character> charList = new LinkedList<>();
+
+        for (char c : str.toCharArray()) {
+            charList.add(c);
+        }
+        while (charList.size() > 1) {
+            if (!charList.pollFirst().equals(charList.pollLast())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public static String convertDecimalToBinary(int decimal){
+        LinkedList<Integer> list=new LinkedList<>();
+
+        while(decimal>0){
+            int remainder=decimal%2;
+            list.addFirst(remainder);
+            decimal/=2;
+        }
+        StringBuilder stringBuilder=new StringBuilder();
+
+        for(int digit:list){
+            stringBuilder.append(digit);
+        }
+        return stringBuilder.toString();
     }
 
 }
